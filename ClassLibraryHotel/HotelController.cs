0afbe93d1c24c13f2d6a07b9bc60172327673598
Stian Grønås <42640705+hvl578007@users.noreply.github.com>
2021-02-19 14:42:34 +0100

    la til ein metode for søk av rom

--- ClassLibraryHotel/HotelController.cs
+++ ClassLibraryHotel/HotelController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,25 @@ namespace ClassLibraryHotel
     public class HotelController
     {
 
-        public static List<Room> RetrieveAvaliableRooms(HotelContext hcx)
+        public static List<Room> RetrieveAvaliableRooms(HotelContext hcx, int beds, int quality, DateTime dateStart, DateTime dateEnd)
         {
-            return null;
+            hcx.Rooms.Load();
+            hcx.Reservations.Load();
+            return hcx.Rooms.Local
+                .Where(r => r.NumOfBeds >= beds && r.Size >= quality)
+                .Where(r =>
+                {
+                    bool notConflictingRes = r.Reservations.All(res =>
+                    {
+                        bool isBefore = res.DateStart.CompareTo(dateStart) < 0 && res.DateEnd.CompareTo(dateStart) < 0;
+                        bool isAfter = res.DateStart.CompareTo(dateEnd) > 0 && res.DateEnd.CompareTo(dateEnd) > 0;
+                        return isBefore || isAfter;
+                    });
+                    bool emptyRes = r.Reservations.Count == 0 || r.Reservations == null;
+                    return notConflictingRes || emptyRes;
+                    //var obj = r.Reservations.FirstOrDefault(res => !res.CheckedOut);
+                    //return obj == null;
+                }).ToList();
         }
     }
 }
