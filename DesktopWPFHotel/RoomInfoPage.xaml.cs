198ba1b53443cf02a8adf0e41fcb3ed186fcca14
Stian Grønås <42640705+hvl578007@users.noreply.github.com>
2021-02-22 14:02:41 +0100

    ...

--- DesktopWPFHotel/RoomInfoPage.xaml.cs
+++ DesktopWPFHotel/RoomInfoPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ClassLibraryHotel;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Task = ClassLibraryHotel.Task;
 
 namespace DesktopWPFHotel
 {
@@ -20,9 +23,27 @@ namespace DesktopWPFHotel
     /// </summary>
     public partial class RoomInfoPage : Page
     {
-        public RoomInfoPage()
+        private DbSet<Room> rooms;
+        private DbSet<Task> tasks;
+
+        public RoomInfoPage(HotelContext hcx)
         {
             InitializeComponent();
+
+            rooms = hcx.Set<Room>();
+            tasks = hcx.Set<Task>();
+
+            rooms.Load();
+            tasks.Load();
+
+
+
+            var taskList = from rm in rooms.Local
+                           join tk in tasks.Local
+                           on rm.RoomId equals tk.RoomRoomId
+                           select new { rm.RoomId, rm.NumOfBeds, rm.Size, tk.Info, tk.Note, tk.State, tk.Type };
+
+            RoomList.DataContext = taskList;
         }
     }
 }
