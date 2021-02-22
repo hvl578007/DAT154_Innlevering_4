64867593b463f751bcdcae199cde0d7b74cb78a8
Stian Grønås <42640705+hvl578007@users.noreply.github.com>
2021-02-22 13:57:27 +0100

    bytta til navigationwindow og pages

--- DesktopWPFHotel/MainPage.xaml.cs
+++ DesktopWPFHotel/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassLibraryHotel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,71 @@ namespace DesktopWPFHotel
     /// </summary>
     public partial class MainPage : Page
     {
+        private HotelContext hcx = new HotelContext();
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void UserSearch_Button(object sender, RoutedEventArgs e)
+        {
+
+            if (WebLoginHelper.CheckIfUsernameExists(userNameText.Text, hcx))
+            {
+                //todo bruk reservationswindow heller her!!
+                //new UserReservationWin(hcx, userNameText.Text).Show();
+                this.NavigationService.Navigate(new ReservationsPage());
+                //this.Close();
+            }
+            else
+            {
+                userNameText.Text = "Please enter a valid user.";
+            }
+
+        }
+
+        private void Reservation_Button(object sender, RoutedEventArgs e)
+        {
+            if (true)//validateForm())
+            {
+                //new ReservationsWindow(hcx).Show();
+                ReservationsPage reservationsPage = new ReservationsPage();
+                this.NavigationService.Navigate(reservationsPage);
+            }
+        }
+
+        private void RoomInfo_Button(object sender, RoutedEventArgs e)
+        {
+            //new RoomInfo(hcx).Show();
+            //this.Close();
+            RoomInfoPage roomInfoPage = new RoomInfoPage();
+            this.NavigationService.Navigate(roomInfoPage);
+        }
+
+        private void AllRoms_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void newRes_Button(object sender, RoutedEventArgs e)
+        {
+            //new SearchRooms(hcx);
+            SearchRoomsPage searchRoomsPage = new SearchRoomsPage();
+            this.NavigationService.Navigate(searchRoomsPage);
+        }
+
+        private bool validateForm()
+        {
+            bool output = true;
+            // legge til flere?
+
+            if (userNameText.Text.Length <= 2 || userNameText.Text.Length > 14)
+            {
+                output = false;
+            }
+
+            return output;
         }
     }
 }
