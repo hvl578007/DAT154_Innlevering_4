16cbf462118fd2d9e134145d99d056d287ee7173
Frank <5>
2021-02-23 14:12:13 +0100

    Oppdatert SearchRooms

--- DesktopWPFHotel/SearchRoomsPage.xaml.cs
+++ DesktopWPFHotel/SearchRoomsPage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ClassLibraryHotel;
 
 namespace DesktopWPFHotel
 {
@@ -20,9 +21,32 @@ namespace DesktopWPFHotel
     /// </summary>
     public partial class SearchRoomsPage : Page
     {
+        HotelContext hcx = new HotelContext();
         public SearchRoomsPage()
         {
             InitializeComponent();
+        }
+
+        protected void BookButton_Click(object sender, EventArgs e)
+        {
+            DateTime dateStart = (DateTime)CalendarFrom.SelectedDate;
+            DateTime dateEnd = (DateTime)CalendarTo.SelectedDate;
+
+            if (dateEnd.CompareTo(dateStart) < 0)
+            {
+                DateTime tmp = dateStart;
+                dateStart = dateEnd;
+                dateEnd = tmp;
+            }
+            else if (dateEnd.CompareTo(dateStart) == 0 || dateEnd == DateTime.MinValue || dateStart == DateTime.MinValue)
+            {
+                return;
+            }
+
+            int beds = int.Parse(numberOfBeds.Tag.ToString()); 
+
+
+
         }
     }
 }
