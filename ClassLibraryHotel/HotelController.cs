a6d06967a378ab7162e7c1768ad922792d57eed3
577854 <577854@stud.hvl.no>
2021-02-23 15:26:42 +0100

    ferdig med reservasjoner?

--- ClassLibraryHotel/HotelController.cs
+++ ClassLibraryHotel/HotelController.cs
@@ -30,5 +30,25 @@ namespace ClassLibraryHotel
                     //return obj == null;
                 }).ToList();
         }
+
+        public static List<Room> RetrieveAvaliableRooms(HotelContext hcx, DateTime dateStart, DateTime dateEnd)
+        {
+            hcx.Rooms.Load();
+            hcx.Reservations.Load();
+            return hcx.Rooms.Local
+                .Where(r =>
+                {
+                    bool notConflictingRes = r.Reservations.All(res =>
+                    {
+                        bool isBefore = res.DateStart.CompareTo(dateStart) < 0 && res.DateEnd.CompareTo(dateStart) < 0;
+                        bool isAfter = res.DateStart.CompareTo(dateEnd) > 0 && res.DateEnd.CompareTo(dateEnd) > 0;
+                        return isBefore || isAfter;
+                    });
+                    bool emptyRes = r.Reservations.Count == 0 || r.Reservations == null;
+                    return notConflictingRes || emptyRes;
+                    //var obj = r.Reservations.FirstOrDefault(res => !res.CheckedOut);
+                    //return obj == null;
+                }).ToList();
+        }
     }
 }
--- DesktopWPFHotel/ReservationsPage.xaml.cs
+++ DesktopWPFHotel/ReservationsPage.xaml.cs
@@ -22,23 +22,42 @@ namespace DesktopWPFHotel
     /// </summary>
     public partial class ReservationsPage : Page
     {
+        private HotelContext hcx;
+        private Reservation selectedRes;
+        private Room selectedAvbRoom;
+
         public ReservationsPage(HotelContext hcx)
         {
+
             InitializeComponent();
 
             hcx.Reservations.Load();
+
+            this.hcx = hcx;
+
             var res = hcx.Reservations;
 
             ResList.DataContext = res.Local;
 
             ResList.SelectionChanged += ResList_SelectionChanged;
+            AvailableRooms.SelectionChanged += AvailableRooms_SelectionChanged;
         }
 
-        public ReservationsPage(HotelContext hcx, string user)
+
+        public ReservationsPage(HotelContext hcx, string usr)
         {
             InitializeComponent();
 
+            hcx.Users.Load();
 
+            this.hcx = hcx;
+
+            var user = hcx.Users.Local.FirstOrDefault(u => u.Username.Equals(usr));
+
+            ResList.DataContext = user.Reservations;
+
+            ResList.SelectionChanged += ResList_SelectionChanged;
+            AvailableRooms.SelectionChanged += AvailableRooms_SelectionChanged;
         }
 
         private void ResList_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -47,11 +66,100 @@ namespace DesktopWPFHotel
 
             if ((sender as ListView).SelectedIndex != -1)
             {
-                if (ContentGrid.Visibility == Visibility) ContentGrid.Visibility = Visibility.Visible;
-                var selectedRes = (sender as ListView).SelectedItem as Reservation;
+                if (ContentGrid.Visibility is Visibility.Hidden) ContentGrid.Visibility = Visibility.Visible;
+                selectedRes = (sender as ListView).SelectedItem as Reservation;
                 ResId.Text = ((sender as ListView).SelectedItem as Reservation).ReservationId.ToString();
+                RoomId.Text = ((sender as ListView).SelectedItem as Reservation).Room.RoomId.ToString();
+
+                if (selectedRes.CheckedIn == true)
+                {
+                    CheckIn.Content = "Check out";
+                }
+                else
+                {
+                    CheckIn.Content = "Check In";
+                }
+
+                AvbRoomsTxt.Visibility = Visibility.Hidden;
+                AvailableRooms.Visibility = Visibility.Hidden;
             }
 
+        }
+
+        private void AvailableRooms_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //throw new NotImplementedException();
+
+            if((sender as ListView).SelectedIndex != -1)
+            {
+                selectedAvbRoom = (sender as ListView).SelectedItem as Room;
+                ChangeRoomBtn.Visibility = Visibility.Visible;
+            }
+
+        }
+
+        private void ChangeRoom_Click(object sender, RoutedEventArgs e)
+        {
+            selectedRes.RoomRoomId = selectedAvbRoom.RoomId;
+            hcx.SaveChanges();
+
+            AvailableRooms.DataContext = null;
+
+            RoomId.Text = selectedRes.RoomRoomId.ToString();
+
+            AvailableRooms.Visibility = Visibility.Hidden;
+            ChangeRoomBtn.Visibility = Visibility.Hidden;
+            AvbRoomsTxt.Visibility = Visibility.Hidden;
+        }
+
+        private void AvbRooms_Click(object sender, RoutedEventArgs e)
+        {
+            //søk på ledige rom og så bytt til valgt rom
+            List<Room> availableRooms = HotelController.RetrieveAvaliableRooms(hcx, selectedRes.DateStart, selectedRes.DateEnd);
+
+            AvailableRooms.DataContext = availableRooms;
+
+            AvbRoomsTxt.Visibility = Visibility.Visible;
+            AvailableRooms.Visibility = Visibility.Visible;
+
+        }
+
+        private void CheckIn_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (selectedRes.CheckedIn == false)
+            {
+                selectedRes.CheckedIn = true;
+                hcx.SaveChanges();
+
+                ResList.DataContext = null;
+
+                hcx.Reservations.Load();
+                
+                var oppdatertRes = hcx.Reservations;
+                
+                ResList.DataContext = oppdatertRes.Local;
+
+            }
+            else
+            {
+                selectedRes.CheckedOut = true;
+
+                //ny cleaning task
+                selectedRes.Room.Tasks.Add(new ClassLibraryHotel.Task { 
+                    Note="", 
+                    Info="Cleaning", 
+                    State=0, 
+                    Type=0, 
+                    RoomRoomId=selectedRes.Room.RoomId }
+                );
+
+                hcx.SaveChanges();
+
+                //skriv at en er sjekket ut?
+
+                //slette reservasjon?
+            }
         }
     }
 }
